cbf4f7cb65f3ffd3d4c8b98e19967ca2da8a69e1
Николай Войцехов <froust87@gmail.com>
2021-03-04 11:49:06 +0300

    v0.2

--- Assets/Scripts/Menu/ButtonManager.cs
+++ Assets/Scripts/Menu/ButtonManager.cs
@@ -8,18 +8,19 @@ using UnityEngine.SceneManagement;
 public class ButtonManager : MonoBehaviour
 {
     public Sprite[] lan;
-    public GameObject sett;
+    public GameObject[] sett;
     public GameObject[] button;
     private Image butt_language;
     private AudioSource audio;
     public AudioClip click;
     private RectTransform panl;
     public int speed;
-    private bool panel; // открыты ли настройки
+    private bool panel,ch_panel; // открыты ли настройки
 
     public void Start()
     {
         panel = false;
+        ch_panel = false;
         butt_language = button[4].GetComponent<Image>();
         if(MenuManager.lang == "RU") 
         {
@@ -48,7 +49,7 @@ public class ButtonManager : MonoBehaviour
         audio = button[1].GetComponent<AudioSource>();
         audio.volume = MenuManager.volume/100f;
         audio.PlayOneShot(click);
-        panl = sett.GetComponent<RectTransform>();
+        panl = sett[0].GetComponent<RectTransform>();
         panel = true;
     }
     private void FixedUpdate()
@@ -61,17 +62,39 @@ public class ButtonManager : MonoBehaviour
                 panl.offsetMax += new Vector2(panl.offsetMax.x, speed);
             }
         }
+        if(ch_panel)
+        {
+            if (panl.offsetMin.y != 0)
+            {
+                panl.offsetMin -= new Vector2(panl.offsetMin.x, speed);
+                panl.offsetMax -= new Vector2(panl.offsetMax.x, speed);
+            }
+        }
     }
     public void Back()
     {
+        if(panel)
+        {
         MenuManager.Save_Sett();
         audio = button[3].GetComponent<AudioSource>();
         audio.volume = MenuManager.volume/100f;
         audio.PlayOneShot(click);
-        panl = sett.GetComponent<RectTransform>();
+        panl = sett[0].GetComponent<RectTransform>();
         panel = false;
         panl.offsetMin = new Vector2(panl.offsetMin.x, -1200);
         panl.offsetMax = new Vector2(panl.offsetMax.x, -1200);
+        }
+        else if(ch_panel)
+        {
+            MenuManager.Save_Sett();
+            audio = button[3].GetComponent<AudioSource>();
+            audio.volume = MenuManager.volume/100f;
+            audio.PlayOneShot(click);
+            panl = sett[1].GetComponent<RectTransform>();
+            ch_panel = false;
+            panl.offsetMin = new Vector2(panl.offsetMin.x, 1200);
+            panl.offsetMax = new Vector2(panl.offsetMax.x, 1200);
+        }
     }
     // Выход
     public void Exit()
@@ -95,6 +118,15 @@ public class ButtonManager : MonoBehaviour
         //Cursor.lockState = CursorLockMode.Locked;
     }
     
+
+    public void Charters()
+    {
+        audio = button[1].GetComponent<AudioSource>();
+        audio.volume = MenuManager.volume/100f;
+        audio.PlayOneShot(click);
+        panl = sett[1].GetComponent<RectTransform>();
+        ch_panel = true;
+    }
     public void Language()
     {
         Debug.Log(MenuManager.lang);
