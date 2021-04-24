ae274d6f42a4b0b581e0065f17d62a052fd7f0da
Николай Войцехов <froust87@gmail.com>
2021-04-24 10:47:34 +0300

    v 0.12
    LoadScreen;
    Helper

---
+++ Assets/Scripts/Menu/LoadLevel.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoadLevel : MonoBehaviour
+{
+    public Slider slider;
+    void Start()
+    {
+      StartCoroutine(LoadAsync());   
+    }
+    IEnumerator LoadAsync()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(2);
+        while(!asyncLoad.isDone)
+        {
+            slider.value = asyncLoad.progress;
+            yield return null;
+        }
+        yield return new WaitForSeconds(4.1f);
+    }
+}
